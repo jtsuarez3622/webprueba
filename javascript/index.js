b95029ef7ecaeadb110724cf738856e86c0e3818
jtsuarez3622 <jtsuarez.ara@gmail.com>
2022-11-01 20:31:36 -0300

    agrego java

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+let nombre = "Jorge";
+let apellido = "Suarez";
+let edad = 33;
+let mascota= "No";
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(mascota);
+console.log(nombre + " " + apellido + " " + edad + " " + mascota)

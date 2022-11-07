8be7c8707f190dcf9a9bd24dbd7428b7e33ef25a
jtsuarez3622 <jtsuarez.ara@gmail.com>
2022-11-07 20:57:48 -0300

    subo pagina con elemento de bootstrap

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,89 @@
 let nombre = "Jorge";
 let apellido = "Suarez";
 let edad = 33;
-let mascota= "No";
+let mascota = "No";
 console.log(nombre);
 console.log(apellido);
 console.log(edad);
 console.log(mascota);
-console.log(nombre + " " + apellido + " " + edad + " " + mascota)
+console.log(nombre + " " + apellido + " " + edad + " " + mascota);
+
+/* let entrada = prompt("Ingrese su nombre");
+let salida = entrada + " " + "tomado";
+alert(salida); */
+
+/* let a = 1 */
+
+/* if (a>5) {
+    console.log("El numero es mayor a 5");
+} else if (a<5) {
+    console.log("el numero es menor a 5");
+} else {console.log ("El numero es igual a 5");
+} */
+
+/* if (a < 5) { 
+    if (a === 1) { 
+        alert("el numero es menor a 5 y es 1");
+     } else {
+         alert("el numero es menor a 5, pero no es 1"); 
+        }
+     } */
+
+/* let años = 15;
+let esMayorEdad = años > 18 ? true:false;
+console.log(esMayorEdad) */
+
+/* let entrada = prompt ("ingrese numero");
+if (entrada>1000) {
+    alert("el numero es mayor a 1000");
+} */
+
+/* let entrada = prompt ("ingresa UN NUMERO"); */
+/* if (entrada=="HOLA") {
+    console.log("El texto ingresado es HOLA");
+} else {alert("el texto no es HOLA");
+
+} */
+
+/* if(entrada>=10){ 
+} if(entrada<=50) {
+    alert("el valor ingresado es mayor a 10 y menor q 50")
+} */
+
+/* let typecritos = ["Bitcoin", "Ethereum", "BinanceCoin", "Cardano","Solana"];
+console.log (typecritos);
+console.log (typecritos.length);
+console.log (typecritos[0]); */
+
+/* let ListaChino = ["Leche", "Queso", "Pollo", "Rucula", "Palta"];
+let entrada = prompt ("ingrese un articulo para la lista del chino");
+if (!entrada) {
+    alert ("no ha ingresado ningun articulo");
+
+} else if (ListaChino.includes(entrada)) {
+    alert ("el producto ya esta en la lista");
+
+}else {
+    ListaChino.push(entrada);
+    alert ("tienes q comprar: "+ListaChino);
+    console.log(ListaChino);
+} */
+
+/* let entryNumber = prompt("Ingrese un Valor Numerico: ");
+
+if (entryNumber>10) {
+    console.log("El Nro es mayor a 10");
+    document.write("El Nro es mayor a 10");
+} else if (entryNumber<=10) {
+    console.log("El Nro es menor o igual a 10");
+    document.write("El Nro es menor o igual a 10"); 
+} else {
+    console.log("Usted no ha ingresado un numero");
+    document.write("Usted no ha ingresado un numero");
+} */
+
+
+
+
+
+
